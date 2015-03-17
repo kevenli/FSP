fde55c7e421bc672b9d4031592602ce70da18979
Keven Li <pbleester@gmail.com>
2015-03-17 10:22:19 +0800

    exception testing

--- java/tests/tests/ClientTests.java
+++ java/tests/tests/ClientTests.java
@@ -1,6 +1,7 @@
 package tests;
 
 import static org.junit.Assert.*;
+import junit.framework.Assert;
 
 import org.junit.After;
 import org.junit.AfterClass;
@@ -10,6 +11,7 @@ import org.junit.Test;
 
 import flowy.scheduler.javasdk.Client;
 import flowy.scheduler.javasdk.Task;
+import flowy.scheduler.javasdk.exceptions.TaskAlreadyExistsException;
 
 public class ClientTests {
 
@@ -45,6 +47,22 @@ public class ClientTests {
 		client.connect();
 		Task task = new Task("TestTask", "*/5 * * * * ?");
 		client.registerTask(task);
+	}
+	
+	@Test
+	public void testRegisterTaskWithException() throws Exception{
+		client.connect();
+		Task task = new Task("TestTask", "*/5 * * * * ?");
+		
+		Task task2 = new Task("TestTask", "*/5 * * * * *");
+		client.registerTask(task);
+		
+		try{
+			client.registerTask(task2);
+			fail("show raise exception here");
+		}catch(TaskAlreadyExistsException e){
+			
+		}
 	}
 
 }
