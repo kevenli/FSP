666081d100a3fc9302f7fc533f8e6f547bc85daa
Keven Li <pbleester@gmail.com>
2015-04-14 09:36:36 +0800

    python client receiving msg

--- python/fsp/client.py
+++ python/fsp/client.py
@@ -11,38 +11,63 @@ DEFAULT_PORT = 3092
 
 
 class FSPClientProtocol(Protocol):
+    
+    read_buffer = ""
     def connectionMade(self):
         #self.transport.
         logging.debug("connected to host")
-        
-        
-        connect_request = protocol.fsp_pb2.ConnectRequest()
-        connect_request.client_protocol_version = "FSP_0.0.1"
         
         request = protocol.fsp_pb2.Request() 
         request.type = protocol.fsp_pb2.Request.CONNECT_REQUEST
         request.Extensions[protocol.fsp_pb2.connect_request].client_protocol_version="FSP_0.0.1"
         
-        body = request.SerializeToString()
+        self.send_msg(request)
+        
+    
+    def send_msg(self, msg):
+        body = msg.SerializeToString()
         body_length = len(body)
         length_codec = Base128VarintCodec();
         encoded_length_package = length_codec.encode(body_length)
         
-        self.transport.write(encoded_length_package+ body)
-        
-    
-    def send_msg(self, msg):
-        data = msg.SerializeToString()
-        
-        #data_length = 
-        
-    def compute_varint_length(self, length):
-        pass
-        
+        self.transport.write(encoded_length_package + body)
         
     def set_client(self, client):
         
         self._client = client
+    
+    def dataReceived(self, data):
+        logging.debug('data received')
+        self.read_buffer += data
+        while True:
+            codec = Base128VarintCodec()
+            length_buffer = ""
+            for i in range(5):
+                if len(self.read_buffer)<i+1:
+                    # no enough buffer read, just return wait for next dataReceived
+                    return
+                length_buffer += self.read_buffer[i]
+                if self.read_buffer[i]>0:
+                    break
+                    
+            body_length = codec.decode(length_buffer)
+            if len(self.read_buffer) < len(length_buffer) + body_length:
+                # no enough buffer
+                return
+            
+            response = protocol.fsp_pb2.Response()
+            response.ParseFromString(self.read_buffer[len(length_buffer):len(length_buffer)+body_length])
+            self.read_buffer = self.read_buffer[len(length_buffer)+body_length:]
+            logging.debug(response)
+            self.on_message(response)
+            
+    
+    def on_message(self, response):
+        if response.type == protocol.fsp_pb2.Response.CONNECT_RESPONSE:
+            self._client.on_connect_response()
+        elif response.type == protocol.fsp_pb2.Response.LOGIN_RESPONSE:
+            self._client.on_login_response(response.Extensions[protocol.fsp_pb2.login_response])
+        
         
 
 class FSPClientFactory(ClientFactory):
@@ -52,6 +77,8 @@ class Client():
     
     def __init__(self, hosts, app_key, app_secret):
         self._hosts = tuple(self._parse_hosts(hosts))
+        self._app_key = app_key
+        self._app_secret = app_secret
         
     @staticmethod   
     def _parse_hosts(hosts):
@@ -70,6 +97,7 @@ class Client():
     def got_protocol(self, p):
         logging.debug('connected, got protocol')
         p.set_client(self)
+        self.protocol = p
         
     def connect(self):
         host, port = self._pick_host()
@@ -79,17 +107,37 @@ class Client():
         
         reactor.run()
         
+    def on_connect_response(self):
+        request = protocol.fsp_pb2.Request() 
+        request.type = protocol.fsp_pb2.Request.LOGIN_REQUEST
+        request.Extensions[protocol.fsp_pb2.login_request].app_key=self._app_key
+        request.Extensions[protocol.fsp_pb2.login_request].app_secret=self._app_secret
+        self.protocol.send_msg(request)
+       
+    def on_login_response(self, login_response):
+        pass
+        
+        
+        
 class Base128VarintCodec:
     def encode(self, value):
-        buffer = ""
+        data = ""
         while True:
             if value & ~0x7f == 0:
-                buffer += struct.pack('b', value)
-                return buffer
+                data += struct.pack('b', value)
+                return data
             else:
-                buffer += struct.pack('B', ((value & 0x7f) | 0x80))
+                data += struct.pack('B', ((value & 0x7f) | 0x80))
                 value = value >> 7 
                 
     
-    def decode(self, buffer):
-        raise NotImplementedError()
+    def decode(self, data):
+        byte_index = 0
+        value = 0
+        for c in data:
+            c_value, = struct.unpack('b', c)
+            value |= ((c_value & 0x7f) << (7*byte_index))
+            byte_index += 1
+            
+        return value
+            
--- python/tests/protocoltests.py
+++ python/tests/protocoltests.py
@@ -41,8 +41,42 @@ class Test(unittest.TestCase):
         actual = codec.encode(i)
         self.assertEqual('\x80\x02', actual)
         
+    def test_decode_varint32_1(self):
+        i = 1
+        data = '\x01'
+        codec = Base128VarintCodec()
+        actual = codec.decode(data)
+        self.assertEqual(1, actual)
+        
+    def test_decode_varint32_127(self):
+        data = '\x7F'
+        value = 127
+        codec = Base128VarintCodec()
+        actual = codec.decode(data)
+        self.assertEqual(value, actual)
+        
+    def test_decode_varint32_128(self):
+        data = '\x80\x01'
+        value = 128
+        codec = Base128VarintCodec()
+        actual = codec.decode(data)
+        self.assertEqual(value, actual)
+        
+    def test_decode_varint32_255(self):
+        data = '\xff\x01'
+        value = 255
+        codec = Base128VarintCodec()
+        actual = codec.decode(data)
+        self.assertEqual(value, actual)
+        
+    def test_decode_varint32_256(self):
+        data = '\x80\x02'
+        value = 256
+        codec = Base128VarintCodec()
+        actual = codec.decode(data)
+        self.assertEqual(value, actual)
 
 
 if __name__ == "__main__":
-    import sys;sys.argv = ['', 'Test.test_encode_varint32_256']
+    import sys;sys.argv = ['', 'Test.test_decode_varint32_1']
     unittest.main()
